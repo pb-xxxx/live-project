9857760d0546c4af9d47eb9e40adee62c8382cdb
chcic <60641293+chcic@users.noreply.github.com>
2020-03-15 13:32:36 +0800

    1

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -3,9 +3,11 @@ package dao;
 import java.util.List;
 import pojo.User;
 public interface UserDAO {
-    boolean add(User user);//添加进数据库
-    void setzero(List<User> users);//预约结束后将set置0
-    List<User> select(List<User> users);//返回中签的用户列表
-    List<User> list();//返回本轮预约成功的用户列表
+    boolean isDb(User user);//查询数据库判断是否可预约
+    boolean isList(User user,List<User> users);//查询预约列表判断是否可预约
+    List<User> addList(User user,List<User> users);//把用户添加进列表，预约成功set置1
+    List<User> select(List<User> users);//选出中签的用户列表
+    boolean add(List<User> users);//把中签的用户添加进数据库
+    void end();//数据库里order-1；
     boolean isMatch(int id);//查询是否中奖
 }
